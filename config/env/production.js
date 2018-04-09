ed31384fc2156d718c747a4d167640e4371a1c80
rleandroruiz <leo1998.04.24@hotmail.com>
2018-04-09 19:24:35 -0300

    Se agrego variable para produccion

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,11 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'https://example.com',
+      'https://staging.example.com',
+      'https://rleandroruiz-sailsapp.herokuapp.com/',
+    ],
 
 
     /***************************************************************************
